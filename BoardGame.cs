7c10c98bbbf6b73ef07ae486f25f4d7cdf1b2fc6
khushaal <79563466+khushaal-nandwani@users.noreply.github.com>
2026-08-10 18:11:02 +0530

    completed chapter 2

---
+++ BoardGame.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBgList
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Year { get; set; }
+    }
+}
